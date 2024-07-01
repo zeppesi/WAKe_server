63cc54d89bcecfdc5c875de392fc6bc2ce4e0ee5
G-D4R3 <gdare1999@gmail.com>
2024-07-01 17:57:23 +0900

    users app 추가

--- users/models.py
+++ users/models.py
@@ -1,8 +1,3 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, User
 from django.db import models
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20, unique=True)
+# Create your models here.
