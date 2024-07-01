6621b0160d835f6ce6a29f25aed671d3929ad1ed
G-D4R3 <gdare1999@gmail.com>
2024-07-01 18:11:19 +0900

    [mvp/WS-1] modeling

--- records/models.py
+++ records/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+def product_image_path(instance):
+    return 'zeppesi/WAKe/contents/{}'.format(instance.id)
+
+
+class Content(models.Model):
+    text = models.TextField(blank=False, null=False)
+    image = models.ImageField(upload_to=product_image_path)
+    is_active = models.BooleanField(default=False, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Record(models.Model):
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, unique=True)
