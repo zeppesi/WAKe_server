beef18382e57087d60ecd47620cc02cf9c76c580
G-D4R3 <gdare1999@gmail.com>
2024-07-01 17:57:36 +0900

    records app 추가

--- records/models.py
+++ records/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
-
-def product_image_path(instance):
-    return 'zeppesi/WAKe/contents/{}'.format(instance.id)
-
-
-class Content(models.Model):
-    text = models.TextField(blank=False, null=False)
-    image = models.ImageField(upload_to=product_image_path)
-    is_active = models.BooleanField(default=False, null=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Record(models.Model):
-    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
-    content = models.ForeignKey(Content, on_delete=models.CASCADE)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# Create your models here.
