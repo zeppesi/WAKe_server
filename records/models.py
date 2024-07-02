78f1e7b502f7086a4939418c0936ac555eed86ba
G-D4R3 <gdare1999@gmail.com>
2024-07-02 15:46:13 +0900

    [mvp/WS-4] admin 추가

--- records/models.py
+++ records/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Content(models.Model):
     text = models.TextField(blank=False, null=False)
-    is_active = models.BooleanField(default=False, null=False)
+    is_active = models.BooleanField(default=True, null=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
