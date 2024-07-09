1aec1bc378f97f7091eaa0ac815ec0d2181d0dcc
G-D4R3 <gdare1999@gmail.com>
2024-07-09 15:23:16 +0900

    git action 수정

--- WAKe_server/settings/__init__.py
+++ WAKe_server/settings/__init__.py
@@ -6,10 +6,12 @@ if ENV_SETTINGS_MODE is None:
     ENV_MODE = 'local'
 elif 'dev' in ENV_SETTINGS_MODE:
     ENV_MODE = 'dev'
+elif 'git' in ENV_SETTINGS_MODE:
+    ENV_MODE = 'git'
 else:
     ENV_MODE = 'local'
 
-if ENV_MODE == 'dev':
+if ENV_MODE == 'dev' or ENV_MODE == 'git':
     from WAKe_server.settings.dev import *
 else:
     from WAKe_server.settings.local import *
