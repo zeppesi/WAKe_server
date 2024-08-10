a13df8b3354130c8d7aa6fb391d5b2444fceed56
G-D4R3 <gdare1999@gmail.com>
2024-08-10 15:28:48 +0900

    [kakao/WS-8] token 생성 로직 분리

--- accounts/views.py
+++ accounts/views.py
@@ -4,15 +4,18 @@ import random
 import requests
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import SlidingToken
 
 from WAKe_server.settings import KAKAO_REST_API_KEY, KAKAO_CLIENT_SECRET, KAKAO_CALLBACK_URI
 from accounts.models import User, CommonProfile
+from accounts.utils import token_serializer
 
 KAKAO_TOKEN_API = "https://kauth.kakao.com/oauth/token"
 KAKAO_USER_API = "https://kapi.kakao.com/v2/user/me"
+
 
 class KaKaoLoginViewSet(viewsets.GenericViewSet):
     @action(detail=False, methods=['GET'])
@@ -40,45 +43,26 @@ class KaKaoLoginViewSet(viewsets.GenericViewSet):
         headers = {"Authorization": f"Bearer ${access_token}"}
         user_infomation = requests.get(KAKAO_USER_API, headers=headers).json()  # 받은 access token 으로 user 정보 요청
 
-        data = {'access_token': access_token, 'code': code}
         kakao_account = user_infomation.get('kakao_account')
         email = kakao_account.get('email')
 
-        # 1. 유저가 이미 디비에 있는지 확인하기
+        # 유저가 이미 디비에 있는지 확인
         try:
+            # a. 있으면 토큰 발행
             user = User.objects.get(email=email)
-
             token = token_serializer(user)
-
             return Response(dict(token=token))
 
         except User.DoesNotExist:
-            # 2. 없으면 회원가입하기
-
+            # b. 없으면 유저 및 프로필 생성
             timestamp = int(datetime.datetime.now().timestamp())
             password = str(random.randint(0, timestamp))
-            data = {
-                'email': email,
-                'password': password
-                # 비밀번호는 없지만 validation 을 통과하기 위해서 임시로 사용
-                # 비밀번호를 입력해서 로그인하는 부분은 없으므로 안전함
-            }
-            user = User.objects.create_user(email, password)  # todo: Profile은 나중에 create
+            user = User.objects.create_user(email, password)
             profile = CommonProfile.objects.create(user=user, name=email)
 
-            # 2-1. 회원가입 하고 토큰 만들어서 쿠키에 저장하기
             try:
                 user = User.objects.get(email=email)
                 token = token_serializer(user)
                 return Response(dict(token=token))
             except:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-def token_serializer(user):
-    jwt_token = SlidingToken.for_user(user)
-    rtn = dict(
-        access=str(jwt_token),
-        refresh=str(jwt_token)
-    )
-    return rtn
