9b8b59d1705a2b2a0e766ef0d9912ec806479315
G-D4R3 <gdare1999@gmail.com>
2024-08-10 15:39:27 +0900

    [kakao/WS-8] users api 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from accounts.views import KaKaoLoginViewSet
+from accounts.views import KaKaoLoginViewSet, UserViewSet
 
 app_name = 'accounts'
 
 router = SimpleRouter()
 router.register('kakao', KaKaoLoginViewSet, basename='kakao')
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include((router.urls, 'accounts')))
