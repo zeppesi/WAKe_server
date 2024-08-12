26d850a6ff3b978b445d5c1e706de3dd272a3623
G-D4R3 <gdare1999@gmail.com>
2024-08-12 15:26:36 +0900

    [kakao/WS-8] middleware set cookie

--- WAKe_server/settings/base.py
+++ WAKe_server/settings/base.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "WAKe_server.middleware.CustomAuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
