24a99f679f90633eac71e36876383890d3367d85
G-D4R3 <gdare1999@gmail.com>
2024-10-01 15:36:51 +0900

    [refactor/WS-12] apiview -> viewset

--- records/urls.py
+++ records/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from records.views import RecordListAPIView, RecordCreateAPIView, ContentViewSet
+from records.views import RecordListAPIView, RecordCreateAPIView, ContentViewSet, RecordViewSet, RandomContentAPIView
 
 app_name = 'records'
 
 router = SimpleRouter()
+router.register("", RecordViewSet, basename="record")
 router.register("content", ContentViewSet, basename="content")
 
 urlpatterns = router.urls
@@ -13,4 +14,5 @@ urlpatterns = router.urls
 urlpatterns += [
     path("records/list/", RecordListAPIView.as_view(), name="record_list"),
     path("records/create/", RecordCreateAPIView.as_view(), name="record_create"),
+    path("contents/random/", RandomContentAPIView.as_view(), name="content_random"),
 ]
