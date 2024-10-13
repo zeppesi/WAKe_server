5eb2f6df3dadfa0f456349de6a76fe00e9c2a3d8
G-D4R3 <gdare1999@gmail.com>
2024-10-13 17:54:45 +0900

    serializer 변경 및 deprecated api 제거

--- records/urls.py
+++ records/urls.py
@@ -1,18 +1,12 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from records.views import RecordListAPIView, RecordCreateAPIView, ContentViewSet, RecordViewSet, RandomContentAPIView
+from records.views import ContentViewSet, RecordViewSet
 
 app_name = 'records'
 
 router = SimpleRouter()
-router.register("", RecordViewSet, basename="record")
+router.register("", RecordViewSet, basename="")
 router.register("content", ContentViewSet, basename="content")
 
 urlpatterns = router.urls
-
-urlpatterns += [
-    path("records/list/", RecordListAPIView.as_view(), name="record_list"),
-    path("records/create/", RecordCreateAPIView.as_view(), name="record_create"),
-    path("contents/random/", RandomContentAPIView.as_view(), name="content_random"),
-]
--- records/views.py
+++ records/views.py
@@ -57,6 +57,11 @@ class RecordViewSet(
     serializer_class = RecordListSerializer
     permission_classes = [AllowAny]
 
+    def get_serializer_class(self):
+        if self.action in ["create"]:
+            return CreateRecordSerializer
+        return RecordListSerializer
+
     def get_target_dates(self) -> (datetime.date, datetime.date):
         q_target_date = self.request.query_params.get('target_date')
         if q_target_date:
@@ -145,117 +150,3 @@ class RecordViewSet(
         record_serializer = RecordSerializer(record)
 
         return Response(record_serializer.data)
-
-
-# deprecated under
-
-
-class RandomContentAPIView(GenericAPIView):
-    serializer_class = ContentSerializer
-
-    def get_prev_filter(self) -> Q:
-        prev = self.request.query_params.get('prev')
-        if not prev:
-            return Q()
-        return ~Q(id=int(prev))
-
-    @swagger_auto_schema(deprecated=True, operation_description="to [GET] api/records/content/random/")
-    def get(self, request: Request):
-        prev_filter = self.get_prev_filter()
-        queryset = Content.objects.filter(
-            prev_filter
-        ).order_by(
-            '?'
-        ).first()
-
-        serializer = self.get_serializer(queryset)
-        return Response(serializer.data)
-
-
-class RecordCreateAPIView(CreateAPIView):
-    serializer_class = RecordCreateSerializer
-    permission_classes = [IsAuthenticated]
-
-    @swagger_auto_schema(
-        deprecated=True,
-        operation_description="to [POST] api/records/"
-    )
-    def post(self, request: Request, *args, **kwargs):
-        serializer: RecordCreateSerializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        username = serializer.validated_data.get('username')
-        content_id = serializer.validated_data.get('content_id')
-        text = serializer.validated_data.get('text')
-
-        profile = CommonProfile.objects.get(name=username)
-
-        record = Record.objects.create(
-            content_id=content_id,
-            profile=profile,
-            text=text
-        )
-
-        record_serializer = RecordSerializer(record)
-
-        return Response(record_serializer.data)
-
-
-class RecordListAPIView(ListAPIView):
-    serializer_class = RecordListSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_target_dates(self) -> (datetime.date, datetime.date):
-        q_target_date = self.request.query_params.get('target_date')
-        if q_target_date:
-            target_date_end = datetime.datetime.strptime(q_target_date, '%Y-%m-%d').date()
-        else:
-            target_date_end = datetime.datetime.today().astimezone(tz=KST).date()
-        target_date_st = target_date_end - datetime.timedelta(days=6)
-        return target_date_st, target_date_end
-
-    def get_queryset(self):
-        target_date_st, target_date_end = self.get_target_dates()
-
-        queryset = Record.objects.filter(
-            profile__user=self.request.user
-        ).annotate(
-            record_date=TruncDate('created_at', tzinfo=KST)
-        ).filter(
-            record_date__gte=target_date_st,
-            record_date__lte=target_date_end
-        ).select_related(
-            'content'
-        ).order_by(
-            'created_at'
-        )
-
-        return queryset
-
-    def list(self, request, *args, **kwargs):
-        target_date_st, target_date_end = self.get_target_dates()
-        queryset = self.filter_queryset(self.get_queryset())
-
-        serializer = RecordSerializer(queryset, many=True)
-        records = serializer.data
-
-        results = []
-        while target_date_st <= target_date_end:
-            results.append(dict(
-                date=target_date_st.strftime('%Y-%m-%d'),
-                records=list(filter(
-                    lambda x: datetime.datetime.fromisoformat(x.get('created_at')).astimezone(
-                        KST).date() == target_date_st,
-                    records
-                ))
-            ))
-            target_date_st += datetime.timedelta(days=1)
-        return Response(results)
-
-    @swagger_auto_schema(
-        deprecated=True,
-        operation_description="to [GET] api/records/",
-        query_serializer=RecordListQuerySerializer
-    )
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
